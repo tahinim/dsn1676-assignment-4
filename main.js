5185e0ce7cc614601a77e4d8a293a5190d23c72f
tahinim <tahinim@hotmail.com>
2015-02-10 05:18:09 +0530

    grapes

--- main.js
+++ main.js
@@ -6,3 +6,41 @@ var blueberries = {
     Carbohydrates: '14.49g',
     protein: '0.74g',
 };
+
+var grapes = {
+    latin: 'Vitis vinifera',
+    growsonshrubs: 'false',
+    energy: '228kJ',
+    Carbohydrates: '18.1g',
+    protein: '0.72g'
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Grapes
+Vitis vinifera
+Grows on shrub: No
+Energy: 288 kJ
+Carbohydrates: 18.1 g
+Protein: 0.72 g
+
+Red currant
+Ribes rubrum
+Grows on shrub: Yes
+Energy: 234 kJ
+Carbohydrates: 13.8 g
+Protein: 1.4 g
