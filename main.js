5646c8971606631fd4b358be4c0a078fc5e03c38
tahinim <tahinim@hotmail.com>
2015-02-10 05:46:47 +0530

    fruits2

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 var blueberries = {
     latin: 'Vaccinium corymbosum',
-    growsonshrubs: 'true',
+    growsonshrubs: true,
     energy: '240kJ',
     carbohydrates: '14.49g',
     protein: '0.74g',
@@ -9,7 +9,7 @@ var blueberries = {
 
 var grapes = {
     latin: 'Vitis vinifera',
-    growsonshrubs: 'false',
+    growsonshrubs: false,
     energy: '228kJ',
     carbohydrates: '18.1g',
     protein: '0.72g'
@@ -17,8 +17,42 @@ var grapes = {
 
 var redcurrent = {
     latin: 'ribes rubrum',
-    growsonshrubs: 'true',
+    growsonshrubs: true,
     energy: '234 kJ',
     carbohydrates: '13.8g',
     protein: '1.4g',
 };
+
+var fruits2 = [blueberries, grapes, redcurrent]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var dinos2 = [trex, stego];
+
+// console.log ( dinos2[1].age )
+
+dinos2.forEach(function (item) {
+    document.write ('<dl>');
+    document.write ('<dt>Age:<dt>');
+    document.write('<dd>'+ item.age +'</dd>');
+    document.write('</dl>');
+
+});
